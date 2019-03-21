d7f2cad1f85cb549b773572a5052e18ec812a273
Gilbert Emerson <emer7@users.noreply.github.com>
2019-03-22 01:53:00 +0800

    Test database

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const { pool } = require('../config/db.js')
+
+var sql_query = 'SELECT * FROM student_info';
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.query(sql_query, (err, data) => {
+		res.send(data.rows);
+	});
 });
 
 module.exports = router;
