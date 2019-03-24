89ecfeea146c52964f92adb98d259718a080041b
Javen <javenseow@gmail.com>
2019-03-24 22:49:31 +0800

    Created query API

--- express/app.js
+++ express/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var query = require('./queries/queries')
 
 var app = express();
 
