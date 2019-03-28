b6b58a5971f61f8797214d216e661939ff34ca10
Gilbert Emerson <emer7@users.noreply.github.com>
2019-03-29 00:51:57 +0800

    Change authentication

--- express/config/authentication.js
+++ express/config/authentication.js
@@ -5,7 +5,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('./db');
 
 passport.use(
-  new LocalStrategy({ usernameField: 'foo', passwordField: 'bar' }, (username, pass, done) => {
+  new LocalStrategy({ usernameField: 'username', passwordField: 'password' }, (username, pass, done) => {
     const query = 'SELECT accountid, username, pass FROM accounts WHERE username=$1';
     const values = [username];
 
@@ -14,7 +14,7 @@ passport.use(
         done(errorQuery);
       }
 
-      if (resultQuery.rows || resultQuery.rows.length > 0) {
+      if (resultQuery || resultQuery.rows || resultQuery.rows.length > 0) {
         const first = resultQuery.rows[0];
         bcrypt.compare(pass, first.pass, (errorBcrypt, resultBcrypt) => {
           if (errorBcrypt) {
