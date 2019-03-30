b7d1bf56b578f65305f6d83510c543a9dd7a30b4
Javen <javenseow@gmail.com>
2019-03-30 15:58:08 +0800

    Added viewAllAvailableItems.js to include a view all availabe items route

--- express/routes/items.js
+++ express/routes/items.js
@@ -9,19 +9,19 @@ router.post('/create', query.createItem);
 // Route to delete items
 router.delete('/delete/:itemSSN', query.deleteItem);
 
-// Route to update items
-router.put('/update/:itemSSN', query.loanedItem);
-
 // Route to search for all items under specific UserSSN
 router.get('/search/all/:loanedByUserSSN', query.searchAllItems);
 
 // Route to search for items that are not loaned
 router.get('/search/available/:loanedByUserSSN', query.searchAvailableItems);
 
-// Route to change loan status when item is returned
+// Route to return item
 router.put('/returned/:transactionSSN', query.returnedItem);
 
 // Route to view the most borrowed item
 router.get('/search/most_borrowed', query.viewMostBorrowedItem);
+
+// Route to view all items
+router.get('/view/all', query.viewAllAvailableItems);
 
 module.exports = router;
