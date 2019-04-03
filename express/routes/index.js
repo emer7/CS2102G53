d8b132d94deea99d7f8ad27097679d3e940eee47
Gilbert Emerson <emer7@users.noreply.github.com>
2019-04-03 10:17:57 +0800

    Query to queries

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const query = require('../queries/queries');
+const queries = require('../queries/queries');
 
 const router = express.Router();
 
 // Route to view all transactions on a user's items
-router.get('/transactions/search/:loanerSSN', query.searchTransactions);
+router.get('/transactions/search/:loanerSSN', queries.searchTransactions);
 
 // Route to view all transaction status of all items he/she has borrowed
-router.get('/transactions/view_all/:borrowerSSN', query.viewAllTransactionStatus);
+router.get('/transactions/view_all/:borrowerSSN', queries.viewAllTransactionStatus);
 
 // Route to accept winning bid
-router.post('/winning_bid/accept', query.acceptWinningBid);
+router.post('/winning_bid/accept', queries.acceptWinningBid);
 
 // Route to view the most expensive minimum bid
-router.get('/bid/view/most_expensive', query.viewMostExpensiveMinBid);
+router.get('/bid/view/most_expensive', queries.viewMostExpensiveMinBid);
 
 // Route to create a new transaction and borrows when a winning bid is accepted
-router.post('/borrow/add', query.addTransactionAndBorrows);
+router.post('/borrow/add', queries.addTransactionAndBorrows);
 
 module.exports = router;
--- express/routes/items.js
+++ express/routes/items.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const query = require('../queries/queries');
+const queries = require('../queries/queries');
 
 const router = express.Router();
 
 // Route to create items
-router.post('/create', query.createItem);
+router.post('/create', queries.createItem);
 
 // Route to delete items
-router.delete('/delete/:itemSSN', query.deleteItem);
+router.delete('/delete/:itemSSN', queries.deleteItem);
 
 // Route to search for all items under specific UserSSN
-router.get('/search/all/:loanedByUserSSN', query.searchAllItems);
+router.get('/search/all/:loanedByUserSSN', queries.searchAllItems);
 
 // Route to search for items that are not loaned
-router.get('/search/available/:loanedByUserSSN', query.searchAvailableItems);
+router.get('/search/available/:loanedByUserSSN', queries.searchAvailableItems);
 
 // Route to return item
-router.put('/returned/:transactionSSN', query.returnedItem);
+router.put('/returned/:transactionSSN', queries.returnedItem);
 
 // Route to view the most borrowed item
-router.get('/search/most_borrowed', query.viewMostBorrowedItem);
+router.get('/search/most_borrowed', queries.viewMostBorrowedItem);
 
 // Route to view all items
-router.get('/view/all', query.viewAllAvailableItems);
+router.get('/view/all', queries.viewAllAvailableItems);
 
 // Route to create bid
-router.post('/bid/create', query.createBid);
+router.post('/bid/create', queries.createBid);
 
 module.exports = router;
